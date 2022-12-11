f6abf0c4ac86ba8a0944fad5b6403b86ca319676
EvilSmurf06 <79266119+EvilSmurf06@users.noreply.github.com>
2022-12-11 11:32:20 +0300

    Conversion partially done

--- Main.java
+++ Main.java
@@ -10,21 +10,18 @@ public class FATİH_EMECEN_S011599 {
         Stack<String> abcReplace = new Stack<>();
         Stack<String> ab = new Stack<>();
         Stack<String> v = new Stack<>();
+        Stack<String> terminalAndNonTerminal = new Stack<>();
         Set<String> hash_Set = new HashSet<>();
+        String startState = ((alphabetRead("START","")).elementAt(0));
         non_terminal = (alphabetRead("NON-TERMINAL","TERMINAL"));
         terminal = (alphabetRead("TERMINAL","RULES"));
-        String x = ((alphabetRead("START","")).elementAt(0));
+        terminal.remove(startState);
         alp = alphabetRead("RULES","START");
-        //alp.add(x+"':"+ x);
-        System.out.println(alp);
         String a = "";
         for(int i = 0; i < alp.size(); i++){
             if( alp.elementAt(i).substring(alp.elementAt(i).lastIndexOf(":")+1).equals("e")) {
-                if (alp.elementAt(i).charAt(0) == x.charAt(0)) {
+
                     a = (alp.elementAt(i).substring(0, 1));
-
-
-
                     for (int j = 0; j < alp.size(); j++) {
                         if (alp.elementAt(j).substring(alp.elementAt(j).lastIndexOf(":") + 1).contains(a)) {
 
@@ -36,7 +33,7 @@ public class FATİH_EMECEN_S011599 {
 
                         }
                     }
-                }
+
                 alp.remove(i);
             }
         }
@@ -48,7 +45,6 @@ public class FATİH_EMECEN_S011599 {
             hash_Set.add(alp.elementAt(i));
 
         }
-        System.out.println(hash_Set);
         char el = ' ';
         for (String element :hash_Set) {
             // Implementing for loop
@@ -80,26 +76,55 @@ for(int i = 0 ; i < non_terminal.size(); i++) {
         for(int i = 0; i < v.size(); i++){
             hash_Set.remove(v.elementAt(i));
         }
-        System.out.println(hash_Set);
-        for(String element: hash_Set){
-            if(element.substring(element.lastIndexOf(":") + 1).length() == 1){
-                if(element.substring(element.lastIndexOf(":") + 1).contains(terminal.elementAt(0)) || element.substring(element.lastIndexOf(":") + 1).contains(terminal.elementAt(1))){
+for(String element: hash_Set){
+    terminalAndNonTerminal.add(element);
+}
 
-                }
-            else if (element.substring(element.lastIndexOf(":") + 1).length() == 2 ){
-                if((element.substring(element.lastIndexOf(":") + 1).contains(terminal.elementAt(0)) || element.substring(element.lastIndexOf(":") + 1).contains(terminal.elementAt(1)))){
 
-                }
+
+/*
+*/
+
+        for(int i = 0; i < terminalAndNonTerminal.size(); i++){
+            if(terminalAndNonTerminal.elementAt(i).substring(terminalAndNonTerminal.elementAt(i).lastIndexOf(":")+1).length() >= 2){
+                for(int j = 0; j < terminal.size(); j++){
+                    if(terminalAndNonTerminal.elementAt(i).substring(terminalAndNonTerminal.elementAt(i).lastIndexOf(":")+1).contains(terminal.elementAt(j))){
+
+                        String replaceTerminal = terminalAndNonTerminal.elementAt(i).substring(terminalAndNonTerminal.elementAt(i).lastIndexOf(":") + 1).replace(terminal.elementAt(j), "X");
+                        hash_Set.remove(terminalAndNonTerminal.elementAt(i));
+                        hash_Set.add(terminalAndNonTerminal.elementAt(i).charAt(0) + ":" + replaceTerminal);
+                        hash_Set.add("X:" + terminal.elementAt(j));
+
+                    }
                 }
 
             }
         }
+        terminalAndNonTerminal.removeAllElements();
+        for(String element: hash_Set){
+
+            terminalAndNonTerminal.add(element);
+        }
+        for(int i = 0; i < terminalAndNonTerminal.size(); i++){
+            if(terminalAndNonTerminal.elementAt(i).substring(terminalAndNonTerminal.elementAt(i).lastIndexOf(":")+1).length() == 3){
+                for(int j = 0; j < terminal.size(); j++){
+                    String replaceNonTerminal = terminalAndNonTerminal.elementAt(i).substring(terminalAndNonTerminal.elementAt(i).lastIndexOf(":") + 1).replace(terminalAndNonTerminal.elementAt(i).substring(2,4), "Y");
+                    hash_Set.remove(terminalAndNonTerminal.elementAt(i));
+                    hash_Set.add(terminalAndNonTerminal.elementAt(i).charAt(0) + ":" + replaceNonTerminal);
+                    hash_Set.add("Y:" + terminalAndNonTerminal.elementAt(i).substring(2,4));
+                }
+
+            }
+        }
+        System.out.println(hash_Set);
+
     }
     public static Stack<String> alphabetRead(String statesName1, String statesName2 ) {
         Stack<String> alphabet = new Stack<>();
         try {
             File myObj = new File("src/G2.txt");
             Scanner myReader = new Scanner(myObj);
+
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 if(data.matches("START")){
